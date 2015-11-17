39f1901975fe9d6d6477ecca7d076ee0dea14410
pxhanus <pxhanus@gmail.com>
2015-11-17 15:04:38 -0600

    Initial commit

---
+++ Hi-LoGame/Form1.Designer.vb
@@ -0,0 +1,107 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblPrompt = New System.Windows.Forms.Label()
+        Me.txtRisk = New System.Windows.Forms.TextBox()
+        Me.btnHigh = New System.Windows.Forms.Button()
+        Me.btnLow = New System.Windows.Forms.Button()
+        Me.lblResult = New System.Windows.Forms.Label()
+        Me.lblTotalPoints = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lblPrompt
+        '
+        Me.lblPrompt.AutoSize = True
+        Me.lblPrompt.Location = New System.Drawing.Point(12, 42)
+        Me.lblPrompt.Name = "lblPrompt"
+        Me.lblPrompt.Size = New System.Drawing.Size(100, 13)
+        Me.lblPrompt.TabIndex = 0
+        Me.lblPrompt.Text = "Enter points to risk: "
+        '
+        'txtRisk
+        '
+        Me.txtRisk.Location = New System.Drawing.Point(118, 39)
+        Me.txtRisk.Name = "txtRisk"
+        Me.txtRisk.Size = New System.Drawing.Size(54, 20)
+        Me.txtRisk.TabIndex = 1
+        '
+        'btnHigh
+        '
+        Me.btnHigh.Location = New System.Drawing.Point(205, 22)
+        Me.btnHigh.Name = "btnHigh"
+        Me.btnHigh.Size = New System.Drawing.Size(75, 23)
+        Me.btnHigh.TabIndex = 2
+        Me.btnHigh.Text = "High"
+        Me.btnHigh.UseVisualStyleBackColor = True
+        '
+        'btnLow
+        '
+        Me.btnLow.Location = New System.Drawing.Point(205, 51)
+        Me.btnLow.Name = "btnLow"
+        Me.btnLow.Size = New System.Drawing.Size(75, 23)
+        Me.btnLow.TabIndex = 3
+        Me.btnLow.Text = "Low"
+        Me.btnLow.UseVisualStyleBackColor = True
+        '
+        'lblResult
+        '
+        Me.lblResult.AutoSize = True
+        Me.lblResult.Location = New System.Drawing.Point(129, 93)
+        Me.lblResult.Name = "lblResult"
+        Me.lblResult.Size = New System.Drawing.Size(0, 13)
+        Me.lblResult.TabIndex = 4
+        '
+        'lblTotalPoints
+        '
+        Me.lblTotalPoints.AutoSize = True
+        Me.lblTotalPoints.Location = New System.Drawing.Point(129, 123)
+        Me.lblTotalPoints.Name = "lblTotalPoints"
+        Me.lblTotalPoints.Size = New System.Drawing.Size(0, 13)
+        Me.lblTotalPoints.TabIndex = 5
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(292, 148)
+        Me.Controls.Add(Me.lblTotalPoints)
+        Me.Controls.Add(Me.lblResult)
+        Me.Controls.Add(Me.btnLow)
+        Me.Controls.Add(Me.btnHigh)
+        Me.Controls.Add(Me.txtRisk)
+        Me.Controls.Add(Me.lblPrompt)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblPrompt As System.Windows.Forms.Label
+    Friend WithEvents txtRisk As System.Windows.Forms.TextBox
+    Friend WithEvents btnHigh As System.Windows.Forms.Button
+    Friend WithEvents btnLow As System.Windows.Forms.Button
+    Friend WithEvents lblResult As System.Windows.Forms.Label
+    Friend WithEvents lblTotalPoints As System.Windows.Forms.Label
+
+End Class
